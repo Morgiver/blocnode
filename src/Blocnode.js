f948b0714f0ba5501c9ac244d4c0eaa577372d49
Morgiver <morgan@morgiver.net>
2020-04-21 09:20:12 +0200

    Adding feature addBloc

--- src/Blocnode.js
+++ src/Blocnode.js
@@ -1,7 +1,28 @@
 
 class Blocnode {
-    constructor() {
+    constructor(Rootbloc = null, rootPath = null) {
+        this.isRoot = true;
 
+        let namespace = {};
+
+        /**
+         * addBloc
+         * @description adding a bloc to the namespace
+         * @param Bloc
+         */
+        this.addBloc = (Bloc) => {
+            if(this.isRoot) {
+                if(blocs.find(item => item === Bloc.name)) {
+                    if(Bloc.Class && Bloc.Class instanceof Blocnode) {
+                        let newBloc = new Bloc.Class(this);
+
+                        if(!namespace[newBloc.name]) {
+                            namespace[newBloc.name] = newBloc;
+                        }
+                    }
+                }
+            } else Rootbloc.addBloc(Bloc);
+        }
     }
 }
 

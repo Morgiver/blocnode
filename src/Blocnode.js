cebf2b16535dbd21ea7f8948dddd660e8acdce40
Morgiver <morgan@morgiver.net>
2020-04-21 09:39:32 +0200

    Adding root application state and handling Process Argv

--- src/Blocnode.js
+++ src/Blocnode.js
@@ -1,9 +1,10 @@
 
 class Blocnode {
-    constructor(Rootbloc = null, rootPath = null) {
+    constructor(Rootbloc = null) {
         this.isRoot = true;
 
         let namespace = {};
+        let state     = {};
 
         /**
          * addBloc
@@ -23,6 +24,52 @@ class Blocnode {
                 }
             } else Rootbloc.addBloc(Bloc);
         }
+
+        if(!Rootbloc) {
+            /**
+             * We are in the RootBloc, here we can set some method to manage all
+             * the blocs.
+             * But first, we need to define the root application state
+             */
+            let args = [];
+
+            process.argv.forEach(item => args.push(item));
+
+            state.nodepath = args[0];
+            args.shift();
+            state.rootpath = args[0];
+            args.shift();
+
+            for(let i in args) {
+                // Checking for command params with "--"
+                let parts = args[i].split('--');
+                // If it's not a params with "--", testing with "-"
+                if(parts.length < 2) parts = args[i].split('-');
+                // removing the first parts, a simple empty string
+                parts.shift();
+
+                if(parts.length > 0) {
+                    // Adding argument to the root application state
+                    let pair = parts[0].split('=');
+                    state[pair[0]] = pair[1] || true;
+                }
+            }
+        } else {
+            /**
+             * We're not in the RootBloc, defining it as it is.
+             */
+            this.isRoot = false;
+        }
+
+        /**
+         * getState
+         * @description Access to the state.
+         * @returns Object
+         */
+        this.getState = () => {
+            if(this.isRoot) return state;
+            else return Rootbloc.getState();
+        };
     }
 }
 
